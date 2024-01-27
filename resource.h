397ce503b298ce139a61fc7e2fd53c08aae06a15
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2024-01-27 12:46:51 +0700

    as is straight outta 2007 🥲

---
+++ resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PMT Viewer.rc
+//
+#define IDD_MAIN                        9
+#define IDE_FILE                        1001
+#define IDB_OPEN                        1002
+#define IDB_PREVIOUS                    1004
+#define IDB_NEXT                        1005
+#define IDC_TITLE                       1028
+#define IDB_LAST                        1032
+#define IDB_FIRST                       1033
+#define IDC_ES_DESCRIPTORS              1034
+#define IDC_PROGRAM_DESCRIPTORS         1035
+#define IDE_TABLE_ID                    1041
+#define IDE_SECTION_SYNTAX_INDICATOR    1042
+#define IDE_SECTION_LENGTH              1043
+#define IDE_PROGRAM_NUMBER              1044
+#define IDE_VERSION_NUMBER              1045
+#define IDE_CURRENT_NEXT_INDICATOR      1046
+#define IDE_SECTION_NUMBER              1047
+#define IDE_LAST_SECTION_NUMBER         1048
+#define IDE_PCR_PID                     1049
+#define IDE_PROGRAM_INFO_LENGTH         1050
+#define IDE_CRC                         1051
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1055
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
